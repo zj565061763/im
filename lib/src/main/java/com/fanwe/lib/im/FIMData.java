e53a7ea4ec591ad6e6f3bfa6d429e0654b15513b
zjun <565061763@qq.com>
2017-11-23 11:04:43 +0800

    重构

--- lib/src/main/java/com/fanwe/lib/im/FIMData.java
+++ lib/src/main/java/com/fanwe/lib/im/FIMData.java
@@ -8,12 +8,14 @@ public interface FIMData<T>
 {
     /**
      * 返回数据类型
+     *
      * @return
      */
     int getType();
 
     /**
      * 解析为第三方SDK的消息
+     *
      * @return
      * @throws Exception
      */
