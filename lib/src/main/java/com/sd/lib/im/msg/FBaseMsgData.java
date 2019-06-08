1e971d2c47abae8e2e910305e7d051fb5d9422de
zhengjun <565061763@qq.com>
2019-06-08 16:18:12 +0800

    FBaseMsgData

--- lib/src/main/java/com/sd/lib/im/msg/FBaseMsgData.java
+++ lib/src/main/java/com/sd/lib/im/msg/FBaseMsgData.java
@@ -5,12 +5,20 @@ import com.sd.lib.im.annotation.MsgData;
 @MsgData(type = -1)
 public abstract class FBaseMsgData<M> implements FIMMsgData<M>
 {
-    @Override
-    public final int getType()
+    private int type;
+
+    public FBaseMsgData()
     {
         final MsgData msgData = getClass().getAnnotation(MsgData.class);
         if (msgData == null)
             throw new IllegalArgumentException("(MsgData) annotation was not found in clazz: " + getClass().getName());
-        return msgData.type();
+
+        this.type = msgData.type();
+    }
+
+    @Override
+    public final int getType()
+    {
+        return type;
     }
 }
