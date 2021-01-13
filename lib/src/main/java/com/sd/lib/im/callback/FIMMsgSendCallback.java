55183739ca4b24426821457ba81a02a111727eea
zjun <565061763@qq.com>
2021-01-13 10:59:35 +0800

    FIMMsgSendCallback

--- lib/src/main/java/com/sd/lib/im/callback/FIMMsgSendCallback.java
+++ lib/src/main/java/com/sd/lib/im/callback/FIMMsgSendCallback.java
@@ -2,11 +2,31 @@ package com.sd.lib.im.callback;
 
 import com.sd.lib.im.msg.FIMMsg;
 
+/**
+ * IM消息发送回调
+ */
 public interface FIMMsgSendCallback
 {
+    /**
+     * 调用发送
+     *
+     * @param msg
+     */
     void onSend(FIMMsg msg);
 
+    /**
+     * 发送成功
+     *
+     * @param msg
+     */
     void onSendSuccess(FIMMsg msg);
 
+    /**
+     * 发送失败
+     *
+     * @param msg
+     * @param code
+     * @param desc
+     */
     void onSendError(FIMMsg msg, int code, String desc);
 }
