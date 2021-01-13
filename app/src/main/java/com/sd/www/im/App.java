dc3701307957d41ac4f5b04cd0550d877f90cf84
zjun <565061763@qq.com>
2021-01-13 11:05:25 +0800

    update

--- app/src/main/java/com/sd/www/im/App.java
+++ app/src/main/java/com/sd/www/im/App.java
@@ -8,7 +8,6 @@ import android.app.Application;
 
 public class App extends Application
 {
-
     @Override
     public void onCreate()
     {
--- app/src/main/java/com/sd/www/im/MainActivity.java
+++ app/src/main/java/com/sd/www/im/MainActivity.java
@@ -1,7 +1,7 @@
 package com.sd.www.im;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sd.lib.im.FIMManager;
 import com.sd.lib.im.callback.FIMMsgCallback;
