f356aec7e9a287af7480c9bd4fc9d7c7e9417ece
zjun <565061763@qq.com>
2021-01-13 11:13:02 +0800

    SendMsgParam 新增优先级

--- lib/src/main/java/com/sd/lib/im/common/SendMsgParam.java
+++ lib/src/main/java/com/sd/lib/im/common/SendMsgParam.java
@@ -7,32 +7,30 @@ import com.sd.lib.im.conversation.FIMConversationType;
  */
 public class SendMsgParam
 {
-    /**
-     * 对方id
-     */
+    /** 会话方id */
     public final String peer;
-    /**
-     * 会话类型
-     */
+    /** 会话类型 */
     public final FIMConversationType conversationType;
-    /**
-     * 是否在线消息，即只有用户在线才会接收到，且不会做本地缓存
-     */
+    /** 是否在线消息，即只有用户在线才会接收到，且不会做本地缓存 */
     public final boolean isOnlineMsg;
+    /** 优先级 */
+    public final int priority;
 
     private SendMsgParam(Builder builder)
     {
         this.peer = builder.peer;
         this.conversationType = builder.conversationType;
         this.isOnlineMsg = builder.isOnlineMsg;
+        this.priority = builder.priority;
         // 暂时不做参数校验
     }
 
     public static final class Builder
     {
-        private String peer = null;
-        private FIMConversationType conversationType = null;
-        private boolean isOnlineMsg = false;
+        private String peer;
+        private FIMConversationType conversationType;
+        private boolean isOnlineMsg;
+        private int priority;
 
         public Builder setPeer(String peer)
         {
@@ -49,6 +47,12 @@ public class SendMsgParam
         public Builder setOnlineMsg(boolean isOnlineMsg)
         {
             this.isOnlineMsg = isOnlineMsg;
+            return this;
+        }
+
+        public Builder setPriority(int priority)
+        {
+            this.priority = priority;
             return this;
         }
 
